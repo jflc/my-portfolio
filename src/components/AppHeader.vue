f38ec9e1b0f4d04bf363db401ff9eae5177d9ab6
Joao Cardoso <jflc@ua.pt>
2017-10-31 15:55:33 +0000

    Header improvement

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,17 @@
 <template>
-<header class="mdc-toolbar mdc-toolbar--fixed mdc-toolbar--waterfall mdc-toolbar--flexible mdc-toolbar--flexible-default-behavior mdc-toolbar--flexible-space-maximized">
+<header v-on:MDCToolbar:change="updateData" class="mdc-toolbar mdc-toolbar--fixed mdc-toolbar--waterfall mdc-toolbar--flexible mdc-toolbar--flexible-default-behavior mdc-toolbar--flexible-space-maximized">
   <div class="mdc-toolbar__row">
     <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
       <div class="mdc-toolbar__title">
         <div class="toolbar-avatar">
-          <img src="../assets/avatar.png" width="24" height="24" />
-          <span>João Cardoso</span>
+          <img src="../assets/avatar.png" v-bind:style="{ opacity:  imgOpacity, margin: imgMargin }" />
+          <span v-bind:style="{ margin:  nameMargin }">João Cardoso</span>
         </div>
       </div>
     </section>
     <section class="mdc-toolbar__section">
       <div class="mdc-toolbar__title toolbar-job-title">
-        <span>Software Engineer</span>
+        <span v-bind:style="{ opacity:  jobOpacity }">Software Engineer</span>
       </div>
     </section>
     <section class="mdc-toolbar__section mdc-toolbar__section--align-end" role="toolbar">
@@ -30,6 +30,24 @@ import {
 
 export default {
   name: 'AppHeader',
+  data () {
+    return {
+      imgOpacity: 1,
+      imgMargin: '1em auto',
+      nameMargin: 'auto 0em',
+      jobOpacity: 1
+    };
+  },
+  methods: {
+    updateData: function (event) {
+      let vm = this;
+      const flexibleExpansionRatio = event.detail.flexibleExpansionRatio;
+      vm.imgOpacity = flexibleExpansionRatio > 0 && flexibleExpansionRatio < 1 ? flexibleExpansionRatio : 1;
+      vm.imgMargin = flexibleExpansionRatio +'em auto';
+      vm.nameMargin = 'auto ' +  (1-flexibleExpansionRatio) + 'em';
+      vm.jobOpacity = flexibleExpansionRatio;
+    }
+  },
   mounted() {
     let vm = this;
     vm.headerToolbar = new MDCToolbar(vm.$el);
@@ -48,13 +66,12 @@ export default {
 .toolbar-avatar {
     display: flex;
     flex-direction: column;
-    align-items: center;
+    // align-items: center;
 }
 
 .toolbar-avatar img {
     width: 3em;
     height: 3em;
-    margin: 1em;
     border-radius: 50%;
     // box-shadow: 0 0 2px 2px black, 0 2px 6px 4px rgba(0,0,0,.2);
     border: 2px solid #fff;
@@ -102,6 +119,6 @@ export default {
 .mdc-toolbar--flexible-space-minimized .toolbar-avatar img {
     width: 1em;
     height: 1em;
-    margin: 0 1em 0 0;
 }
+
 </style>
